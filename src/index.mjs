9bea0fa5b17ce7209a39881b5177057d7b81dba1
cyubahiro Richard <cyubahirorichard250@gmail.com>
2025-01-21 14:52:35 -0800

    initial commit

---
+++ src/index.mjs
@@ -0,0 +1,53 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+    { id:1, username: "mugabo", displayName: "Mugabo"},
+    { id:2, username:"kagabo", displayName: "kagabo"},
+    { id:3, username:"keline", displayName: "Keline"},
+    { id:4, username:"leline", displayName: "leline"},
+    { id:5, username:"geline", displayName: "geline"},
+    { id:6, username:"meline", displayName: "meline"},
+    { id:7, username:"celine", displayName: "celine"}
+];
+
+app.get('/', (request, response) => {
+    response.status(201).send({ messsage: 'Hello!'});
+});
+
+app.get('/api/users', (request, response) => {
+    console.log(request.query);
+    const { query: { filter, value } } = request;
+    if(!filter && ! value) return response.send(mockUsers); 
+   
+    if(filter && value) return response.send(
+        mockUsers.filter((user) => user[filter].includes(value))
+    );
+     return response.send(mockUsers)
+
+});
+app.get('/api/products', (request, response) => {
+    response.send([
+        { id:123, name: "chicken wings", price: 12.99},
+
+    ]);
+});
+
+
+app.get('/api/users/:id', (request, response) => {
+    console.log(request.params)
+    const parsedId = parseInt(request.params.id);
+    if(isNaN(parsedId)) return response.status(400).send({msg: "Bad request. Invalid ID."});
+
+    const findUser = mockUsers.find((user) => user.id === parsedId);
+
+    if(!findUser) return response.sendStatus(404);
+    return response.send(findUser)
+})
+
+app.listen(PORT, () => {
+    console.log(`running on port${PORT}`)
+})
