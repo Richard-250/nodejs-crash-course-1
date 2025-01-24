a1d5828c60a0c79ee53358090b9cf33660362a34
cyubahiro Richard <cyubahirorichard250@gmail.com>
2025-01-24 11:48:49 -0800

    refining passport

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
@@ -26,9 +26,28 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes); 
   
-app.post('/api/auth', passport.authenticate('local'), (request, response) => {
-
+app.post('/api/auth',
+     passport.authenticate('local'),
+      (request, response) => {
+  response.sendStatus(200);
 });
+
+app.get('/api/auth/status', (request, response) => {
+    console.log(`Inside /auth/status endpoint`);
+    console.log(request.user);  
+    console.log(request.session);
+    return request.user ? response.send(request.user) : response.sendStatus(401)
+});
+
+app.post('/api/auth/logout', (request, response) => {
+    if (!request.user) return response.sendStatus(401);
+
+    request.logOut((err) => {
+        if (err) return response.sendStatus(400);
+        response.send(200)
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -2,6 +2,24 @@ import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import { mockUsers } from '../utils/constants.mjs';
 
+passport.serializeUser((user, done) => {
+  console.log(`Inside sereialize User`);
+  console.log(user);
+done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  console.log(`Inside Deserialize User`);
+  console.log(`Deserialize user Id:${id} `);
+  try {
+    const findUser = mockUsers.find((user) => user.id === id);
+    if (!findUser) throw new Error("User Not Found");
+    done(null, findUser);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
 export default passport.use(
   new LocalStrategy((username, password, done) => {
     console.log(`username: ${username}`);
@@ -16,4 +34,4 @@ export default passport.use(
     }
   })
 );
- 
+  
